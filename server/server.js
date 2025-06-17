a824585927d9c7efddde2410c95e6f8f116785c8
aau142 <anon2061314@gmail.com>
2025-06-18 04:23:39 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,16 +31,16 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 // ✅ 静的ファイルの公開設定
 app.use(express.static(path.join(__dirname, '../client-mac/build')));
 app.use('/mac', express.static(path.join(__dirname, '../client-mac/build')));
-// app.use('/', express.static(path.join(__dirname, '../client/build')));
+app.use('/', express.static(path.join(__dirname, '../client/build')));
 
 // ✅ HTMLファイルルーティング（React用）
 app.get('/mac/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client-mac/build/index.html'));
 });
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+ });
 
 
 // ✅ /api/analyze（photoの外）
