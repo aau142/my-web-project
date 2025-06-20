6a5be08d2a3a22aa15f976f3d88838e2a75dfc34
aau142 <anon2061314@gmail.com>
2025-06-21 02:11:15 +0900

    Update 2camera.jsx

--- client/src/2camera.jsx
+++ client/src/2camera.jsx
@@ -79,7 +79,7 @@ const Camera = () => {
       {/* ON / OFF ボタン */}
       <div style={{
         position: 'absolute',
-              top: '700px',
+              top: '550px',
   left: '50%',
   transform: 'translateX(-50%)',
   width: 'auto',
