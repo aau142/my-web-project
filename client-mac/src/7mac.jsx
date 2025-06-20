c046125e04a1a579d8c01e861b29e32549a7acd9
aau142 <anon2061314@gmail.com>
2025-06-21 04:14:31 +0900

    Update 7mac.jsx

--- client-mac/src/7mac.jsx
+++ client-mac/src/7mac.jsx
@@ -30,7 +30,7 @@ const SevenMac = ({ url }) => {
         title="年表"
         style={{
           position: 'absolute',
-          top: '52%',           // 位置微調整（画像中央のグレー枠に合わせて）
+          top: '42%',           // 位置微調整（画像中央のグレー枠に合わせて）
           left: '53%',
           transform: 'translate(-50%, -50%)',
           width: '60%',
